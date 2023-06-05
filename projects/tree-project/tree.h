39b1dc923653eb104594dd95ab375d742f6f280b
gceni0401 <134641433+gceni0401@users.noreply.github.com>
2023-06-05 18:37:36 +0000

    Commit.

--- projects/tree-project/tree.h
+++ projects/tree-project/tree.h
@@ -1,4 +1,5 @@
 #ifndef TREE_H
+#define TREE_H
 
 #include <vector>
 #include <string>
@@ -6,25 +7,27 @@
 
 class Node
 {
-    private:
-    int size() const;
-    void print() const;
-    std::string data;
-    std::vector<Node*> children;
-    friend class Tree;
+private:
+
+   int size() const;
+   void print() const;
+   std::string data;
+   std::vector<Node*> children;
+   friend class Tree;   
 };
+
 
 class Tree
 {
     public:
     Tree();
+    void print() const;
     Tree(std::string root_data);
     void add_subtree(const Tree& subtree);
+    
     int size() const;
     
-    void print() const;
-    
-    private:
+    private:   
     Node* root;
 };
 
